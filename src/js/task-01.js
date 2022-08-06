d0ccf47bd4bd0f21c037eb21a2b03f03af3a02c8
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-07 00:07:36 +0300

    1 task comleted

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+// const ulItemEl = document.querySelectorAll(".item");
+
+// console.log(`Number of categories: ${ulItemEl.length}`);
+
+const nameCategories = document.querySelector("#categories");
+const nameCategoriesItem = nameCategories.children;
+console.log(`Number of categories: ${nameCategoriesItem.length}`);
+for (let i = 0; i < nameCategoriesItem.length; i += 1) {
+  console.log("Category:", nameCategoriesItem[i].firstElementChild.textContent);
+  console.log(
+    "Elements:",
+    nameCategoriesItem[i].lastElementChild.children.length
+  );
+}
