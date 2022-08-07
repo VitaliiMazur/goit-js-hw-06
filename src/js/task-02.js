dd0f29206a844e43724c1285113cccf51aa4b4c8
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-07 23:47:20 +0300

    task 2 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arrayIngredients = ingredients.map((ingredient) => {
+  const ingItem = document.createElement("li");
+  console.log(ingItem);
+  ingItem.textContent = ingredient;
+  ingItem.classList.add("item");
+  return ingItem;
+});
+
+const ingEl = document.querySelector("#ingredients");
+
+ingEl.append(...arrayIngredients);
