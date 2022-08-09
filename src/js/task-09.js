579004d2d1e327fc65070a295498258311e118c1
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-09 14:36:24 +0300

    task 9 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", onButtonEl);
+function onButtonEl(event) {
+  event.preventDefault();
+  const randomColor = getRandomHexColor();
+  spanEl.textContent = randomColor;
+  bodyEl.style.backgroundColor = randomColor;
+}
