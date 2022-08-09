0e9d3a64c858a85116969a9ecc3da312a8383218
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-09 13:37:57 +0300

    task 8 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", formEl);
+function formEl(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return console.log("Alert!!! Please fill in all the fields!");
+  } else console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
